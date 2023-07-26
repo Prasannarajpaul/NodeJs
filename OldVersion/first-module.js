449e5431dce31a2f6a21fa62aef54ccc9ac7c15f
Prasannarajpaul <104220676+Prasannarajpaul@users.noreply.github.com>
2023-07-26 15:11:31 +0530

    Create first-module.js

---
+++ OldVersion/first-module.js
@@ -0,0 +1,5 @@
+//user defined module to return date and time
+//a module has one export statement like return in function
+exports.myDate=function(){//it is anonymous function and it is better to use a arrow function
+    return Date();
+};
